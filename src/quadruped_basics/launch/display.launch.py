28a36b12526a6f92d2b08cc1ea84fc9593b9221f
joschmaCYU <joschka.mayer@etu.cyu.fr>
2026-04-14 13:12:50 +0200

    Better dashboard (teleop(ccan change speed) + launch + run). Consistant real robot run. Save load positions !

--- src/quadruped_basics/launch/display.launch.py
+++ src/quadruped_basics/launch/display.launch.py
@@ -1,7 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess  # <-- NOUVEL IMPORT ICI
+from launch.actions import ExecuteProcess
 from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
